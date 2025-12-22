1eae4733301fae72f0b96eeee74bb19443222571
Edson Rodrigues <thecodeverse12@gmail.com>
2025-12-22 09:48:24 +0100

    feat(models): image model created and associated with product

---
+++ app/models/image.py
@@ -0,0 +1,8 @@
+from database import BaseModel
+from peewee import CharField, DateTimeField
+from datetime import datetime
+
+class Image(BaseModel):
+    url = CharField()
+    filename = CharField()
+    created_at = DateTimeField(default=datetime.utcnow)
--- app/models/product.py
+++ app/models/product.py
@@ -1,13 +1,14 @@
 from peewee import CharField, FloatField, ForeignKeyField
 from app.database import BaseModel
 from app.models.category import Category
+from app.models.image import Image
 
 class Product(BaseModel):
     name = CharField()
     description = CharField()
     price = FloatField()
     stock = FloatField()
-    image_url = CharField(null=True)
+    image = ForeignKeyField(Image, null=True, backref="products", on_delete="SET NULL")
     category = ForeignKeyField(Category, backref='products', null=True)
     public_id = CharField(unique=True)
 
@@ -18,7 +19,7 @@ class Product(BaseModel):
             'description': self.description,
             'price': self.price,
             'stock': self.stock,
-            'image_url': self.image_url,
+            'image': self.image,
             'category': self.category.name if self.category else None,
             'public_id': self.public_id
         }
