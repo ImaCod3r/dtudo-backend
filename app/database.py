5c729868b37214845d012e12704352778448ba9f
Edson Rodrigues <thecodeverse12@gmail.com>
2026-01-08 12:30:16 +0100

    fix: db init error

--- app/database.py
+++ app/database.py
@@ -7,16 +7,11 @@ load_dotenv()
 # Configuração do MySQL a partir de variáveis de ambiente
 db = MySQLDatabase(
     os.getenv("DB_NAME", "dtudo"),
-    stale_timeout=300,
-    max_connections=8,
     user=os.getenv("DB_USER", "root"),
     password=os.getenv("DB_PASSWORD", ""),
     host=os.getenv("DB_HOST", "localhost"),
     port=int(os.getenv("DB_PORT", 3306)),
-    charset='utf8mb4',
-    thread_safe=True,
-    autorollback=True,
-    connect_timeout=10
+    charset='utf8mb4'
 )
 
 class BaseModel(Model):
