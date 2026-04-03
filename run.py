984c6a2e0d22709261021ff89ea47843627424e4
Edson Rodrigues <143434065+ImaCod3r@users.noreply.github.com>
2026-04-03 01:56:10 +0100

    Change app run port from 3000 to 5000

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import init_app
 app = init_app()
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=3000, debug=True, threaded=False, use_reloader=False)
+    app.run(host="localhost", port=5000, debug=True, threaded=False, use_reloader=False)
